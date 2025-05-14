ed40b64a800c4e80889f2a8f79314e1d627c533b
Oleg PAVLOV <OPAVLOV@main.orange.md>
2025-05-14 12:28:41 +0300

    add requierments and microsoft app id

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "028d4938-3d2a-410f-8f41-3c7e542e9b0c")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
